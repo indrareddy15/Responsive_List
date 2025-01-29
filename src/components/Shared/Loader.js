2173c5fa6d556b6411848828eb39f1c689065963
indrareddy15 <gantaindrareddy83@gmail.com>
2025-01-29 23:38:45 +0530

    loader

---
+++ src/components/Shared/Loader.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+// import './Loader.css';
+
+const Loader = () => {
+    return <div className="loader">Loading...</div>;
+};
+
+export default Loader;

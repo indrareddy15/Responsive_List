80603285800772438cc60a7b976832c527f92832
indrareddy15 <gantaindrareddy83@gmail.com>
2025-01-29 23:41:19 +0530

    mes

---
+++ src/api/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const getLists = async () => {
+    try {
+        const response = await axios.get('https://apis.ccbp.in/list-creation/lists');
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
